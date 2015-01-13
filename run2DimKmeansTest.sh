b3e318b8b27f7501afb4be5a8f5c26137e59d98d
Mikolaj Markiewicz <kajo100@gmail.com>
2015-01-13 01:55:48 +0100

    Multidim workaround - 'generic script'.

--- run2DimKmeansTest.sh
+++ run2DimKmeansTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-    echo "usage: needs 2 params - baseFileName and 'parameters' [iterations] eg:"
+    echo "usage: needs 2 params - baseFileName and 'parameters' [iterations] [treatAsMultiDimensional] eg:"
     echo "\t$0 2dim_Compound '-ke -i 100 -l 0.1 -g 5'"
     exit 1;
 fi
@@ -9,9 +9,13 @@ fi
 name=$1
 params=$2
 iterations=10
+no2dim=0
 
-if [ $# -eq 3 ]; then
+if [ $# -ge 3 ]; then
     iterations=$3
+fi
+if [ $# -eq 4 ]; then
+    no2dim=1
 fi
 
 l="./test/data/${name}-labels.txt"
@@ -24,7 +28,11 @@ maxId=1
 maxVal=0
 
 for i in `seq 1 $iterations`; do
-    res[$i]=`./app -f $f -o ${o}$i -r ${r}$i $params` #> /dev/null
+    if [ $no2dim -eq 0 ]; then
+        res[$i]=`./app -f $f -o ${o}$i -r ${r}$i $params` #> /dev/null
+    else
+        res[$i]=`./app -f $f -r ${r}$i $params` #> /dev/null
+    fi
     val=`./test/rand/randInd.r ${r}$i $l | sed -e 's/\[1\] //g'`
     echo $val
 
@@ -35,13 +43,21 @@ for i in `seq 1 $iterations`; do
 done
 
 echo
-echo "./app -f $f -o ${o}$i -r ${r}$i $params"
+if [ $no2dim -eq 0 ]; then
+    echo "./app -f $f -o ${o}$maxId -r ${r}$maxId $params"
+else
+    echo "./app -f $f -r ${r}$maxId $params"
+fi
 echo "${res[$maxId]}"
 echo "[1] $maxVal"
 
-mv -f ${o}$maxId ${o}
+if [ $no2dim -eq 0 ]; then
+    mv -f ${o}$maxId ${o}
+fi
 mv -f ${r}$maxId ${r}
 rm -f ${o}?* ${r}?*
 
-./test/2dim/med2dimChart.r $o
+if [ $no2dim -eq 0 ]; then
+    ./test/2dim/med2dimChart.r $o
+fi
 
